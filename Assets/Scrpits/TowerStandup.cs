bcc068adfe923144d197795583915ab2f9cbce33
xyskin <xyskin@163.com>
2017-07-24 11:33:51 +0800

    1

---
+++ Assets/Scrpits/TowerStandup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerStandup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ball")
+        {
+            this.transform.up = Vector3.up;
+            this.transform.position = new Vector3(-90.69019f, 137.7208f, 80.24561f);
+        }
+
+    }
+}
