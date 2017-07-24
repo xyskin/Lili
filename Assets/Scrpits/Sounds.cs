5e2e1d929d3cc763ff166732d80dd2f9204ead6f
xyskin <xyskin@163.com>
2017-07-24 23:39:03 +0800

    Neko 23:38

---
+++ Assets/Scrpits/Sounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour {
+	public TubeSound o;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "ball")
+		{
+			Destroy(this.gameObject, 0);
+			o.Sound = true;
+		}
+	}
+}
---
+++ Assets/Scrpits/TubeSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TubeSound : MonoBehaviour {
+	public bool Sound;
+	public AudioSource audio;
+	public Image image;
+	// Use this for initialization
+	void Start () {
+		Sound = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Sound == true && !audio.isPlaying)
+		{
+			Debug.Log("sound out");
+			image.color = new Color(0, 0, 0, 1);
+			audio.Play();
+		}
+		if (Sound == false && audio.isPlaying)
+		{
+			Debug.Log("sound end");
+			image.color = new Color(0, 0, 0, 0);
+			audio.Stop();
+		}
+	}
+}
